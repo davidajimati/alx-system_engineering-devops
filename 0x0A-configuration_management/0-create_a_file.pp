2007ca3b02c8b065709d3ec76c0672aa6cf22f96
davidajimati <ajimatidavid@gmail.com>
2023-04-21 13:03:59 +0100

    0-create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# shell manisfest to create a file in /etc
-file {'/etc/school':
-  ensure  =>'file',
-  mode    =>'0744',
-  owner   =>'www-data',
-  group   =>'www-data',
-  content =>'I love Puppet',
+# creates a file in the /tmp directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
