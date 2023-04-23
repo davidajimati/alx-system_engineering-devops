18ad412ad462fb02646887ea4b5b3cb6ddc55eca
davidajimati <ajimatidavid@gmail.com>
2023-04-23 18:30:34 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,5 @@
 # sets up the config file to enable passwordless login
 file {'~/.ssh/config'
-  ensure  => 'file',
-  content => @(EOF)
-User ubuntu
-  Hostname 54.82.29.159
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no
-EOF
+  ensure  => 'present',
+  content => 'User ubuntu\  Hostname 54.82.29.159\  IdentityFile ~/.ssh/school\  PasswordAuthentication no',
 }
