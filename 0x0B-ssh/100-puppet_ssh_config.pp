e81671eb6d577a4627eb4e2a4635da76265f5d74
davidajimati <ajimatidavid@gmail.com>
2023-04-23 18:24:08 +0100

    'ensure' argument changed to 'file'

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # sets up the config file to enable passwordless login
 file {'~/.ssh/config'
-  ensure  => 'present',
+  ensure  => 'file',
   content => @(EOF)
 User ubuntu
   Hostname 54.82.29.159
