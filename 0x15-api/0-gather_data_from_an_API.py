41aeb14aab282f6693d191ef68ae67da22b54b51
davidajimati <ajimatidavid@gmail.com>
2023-05-30 23:02:49 +0100

    0-gather_data_from_an_API.py | experimenting with urllib properties

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,10 +3,31 @@
 using REST API, for a given employee ID,
 returns information about his/her TODO list progress
 '''
+import sys
+import json
+from urllib import parse, request
+
+
+def get_todo(id):
+    """
+    gets the todo list of a user.
+    """
+
+    params = {"userId": id}
+    query = parse.urlencode(params)
+
+    url = "https://jsonplaceholder.typicode.com/todos" + "?" + query
+
+    response = request.urlopen(url)     #opens url
+    html = response.read()              # converts the response to human readable format
+    jsonFormat = json.load(html)        # Json format
+    print(jsonFormat)
+
+    # print("Employee {} is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):")
 
 
 if __name__ == "__main__":
     """
     Executes the code if __name__ is __main__
     """
-    from urllib import request, parse
+    get_todo(sys.argv[1])
