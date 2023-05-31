3bd027ceb248a13c01b7299d2f4c7bde4b49f90b
davidajimati <ajimatidavid@gmail.com>
2023-05-31 12:35:04 +0100

    0-gather_data_from_an_API.py broken down ti functions

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,26 +8,46 @@ import json
 from urllib import parse, request
 
 
+def get_name(id):
+    """
+    This function retrieves the name of a user
+    """
+    data = {"id": id}
+    values = parse.urlencode(data).encode('utf-8')
+    url = 'https://jsonplaceholder.typicode.com/users'
+
+    req1 = request.Request(url, data=values, method="GET")
+    resp_name = request.urlopen(req1)
+    user_name = json.loads(resp_name.read().decode())
+    return (user_name[0]["name"])
+
+
 def get_todo(id):
     """
     gets the todo list of a user.
     """
-
     params = {"userId": id}
-    query = parse.urlencode(params)
+    query = parse.urlencode(params).encode(
+        'utf-8')     # encodes the parameters to bytes
+    url = "https://jsonplaceholder.typicode.com/todos"
 
-    url = "https://jsonplaceholder.typicode.com/todos" + "?" + query
+    # form standard url that can be accepted by the target API
+    req = request.Request(url, data=query, method="GET")
+    # query the API by opening the link
+    response = request.urlopen(req)
+    # read, decode and load the response as a json object
+    json_data = json.loads(response.read().decode())
+    return (json_data)
 
-    response = request.urlopen(url)     #opens url
-    html = response.read()              # converts the response to human readable format
-    jsonFormat = json.load(html)        # Json format
-    print(jsonFormat)
 
-    # print("Employee {} is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):")
+def run_all(id):
+    name = get_name(id)
+    # todo = get_todo(id)
 
+    print(name)
 
 if __name__ == "__main__":
     """
     Executes the code if __name__ is __main__
     """
-    get_todo(sys.argv[1])
+    run_all(sys.argv[1])
