f022a4b7c2e5749895f3b220017bf481f4e9c394
davidajimati <ajimatidavid@gmail.com>
2023-05-31 16:13:22 +0100

    get_name function updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,60 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
+'''
+using REST API, for a given employee ID,
+returns information about his/her TODO list progress
+'''
+
+import json
 import sys
+from urllib import parse, request
+
+
+def get_name(id):
+    """
+    This function retrieves the name of a user
+    """
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+    resp_name = request.urlopen(url)
+    user_name = json.loads(resp_name.read().decode())
+    return user_name.get("name")
+    # return (user_name)
+
+
+def get_todo(id):
+    """
+    gets the todo list of a user.
+    """
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+
+    response = request.urlopen(url)
+    # read, decode and load the response as a json object
+    json_data = json.loads(response.read().decode())
+    return (json_data)
+
+
+def run_all(id):
+    """
+    entry point into the program
+    """
+    name = get_name(id)
+    todo_list = get_todo(id)
+    done = 0
+    total = 0
+    completed = []
+
+    for item in todo_list:
+        total += 1
+        if item.get("completed"):
+            completed.append(item["title"])
+            done += 1
+
+    print("Employee {} is done with tasks({}\\{}):" .format(name, done, total))
+    print("\n".join("\t" + item for item in completed))
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    """
+    Executes the code if __name__ is __main__
+    """
+    run_all(sys.argv[1])
