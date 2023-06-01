63e56b655906da105d15d31bbde2af1bb7d3aea3
davidajimati <ajimatidavid@gmail.com>
2023-06-01 10:08:14 +0100

    2-export_to_JSON.py todo list retrieved

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 """
-exports user data in scrip 0 to a json file
+exports user data defined in script 0 to a json file
 """
 
-import
+import json
+from sys import argv
+from urllib import request
+
+
+def extract_to_json(id):
+    """
+    Extracts user information and exports it to json
+    """
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    # name_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+    response = json.loads(request.urlopen(url).read())
+    data = {id: response}
+    print(data)
+
+
+if __name__ == "__main__":
+    """runs the program if name is main"""
+    extract_to_json(argv[1])
