5af7e7917c33116a3164b9d0c64836d2692de2fb
davidajimati <ajimatidavid@gmail.com>
2023-06-02 09:42:37 +0100

    2-export_to_JSON.py finished

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,20 +13,23 @@ def extract_to_json(id):
     Extracts user information and exports it to json
     """
     url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+
     filename = "{}.json".format(id)
-
     response = json.loads(request.urlopen(url).read())
-
-    task = response.get('title')
-    completed = response.get('completed')
-    username = response.get('username')
+    username = json.loads(request.urlopen(name_url).read()).get('username')
 
     data = []
+    for item in response:
+        task = item.get('title')
+        completed = item.get('completed')
+        line = {"task": task, "completed": completed, "username": username}
+        data.append(line)
 
-    data = {id: data}
+    result = {id: data}
 
-    # with open(filename, 'w') as file:
-    #     json.dump(file, response)
+    with open(filename, 'w') as file:
+        json.dump(response, file)
 
 
 if __name__ == "__main__":
